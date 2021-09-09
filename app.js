fd13d44e6e4a5f5fe2293be885860c27ba60bdca
AllIThinkAboutIsKarma <martinmarescr@gmail.com>
2021-09-09 21:10:40 +0200

    Summary landingPage
    Se terminò la landing page, se agregò un slider de dos partes con 5 temas cada una y se agregò el fondo a las categorias restantes. Queda pendiente agregar los html de cada categoria

--- app.js
+++ app.js
@@ -3,6 +3,144 @@
     $("#container").addClass("background-history");
 });*/
 
+function elemento5(){
+    document.getElementById("c5").style.display="none";
+}
+
+function elemento6(){
+    document.getElementById("c6").style.display="none";
+}
+
+function elemento7(){
+    document.getElementById("c7").style.display="none";
+}
+
+function elemento8(){
+    document.getElementById("c8").style.display="none";
+}
+
+function elemento9(){
+    document.getElementById("c9").style.display="none";
+}
+
+elemento5();
+elemento6();
+elemento7();
+elemento8();
+elemento9();
+
+function Derecha() {
+    function elemento5(){
+        document.getElementById("c5").style.display="flex";
+    }
+    
+    function elemento6(){
+        document.getElementById("c6").style.display="flex";
+    }
+    
+    function elemento7(){
+        document.getElementById("c7").style.display="flex";
+    }
+    
+    function elemento8(){
+        document.getElementById("c8").style.display="flex";
+    }
+    
+    function elemento9(){
+        document.getElementById("c9").style.display="flex";
+    }
+    
+    elemento5();
+    elemento6();
+    elemento7();
+    elemento8();
+    elemento9();
+
+    function elemento0(){
+        document.getElementById("c0").style.display="none";
+    }
+    
+    function elemento1(){
+        document.getElementById("c1").style.display="none";
+    }
+    
+    function elemento2(){
+        document.getElementById("c2").style.display="none";
+    }
+    
+    function elemento3(){
+        document.getElementById("c3").style.display="none";
+    }
+    
+    function elemento4(){
+        document.getElementById("c4").style.display="none";
+    }
+    
+    elemento0();
+    elemento1();
+    elemento2();
+    elemento3();
+    elemento4();
+}
+
+function Izquierda() {
+    function elemento5(){
+        document.getElementById("c5").style.display="none";
+    }
+    
+    function elemento6(){
+        document.getElementById("c6").style.display="none";
+    }
+    
+    function elemento7(){
+        document.getElementById("c7").style.display="none";
+    }
+    
+    function elemento8(){
+        document.getElementById("c8").style.display="none";
+    }
+    
+    function elemento9(){
+        document.getElementById("c9").style.display="none";
+    }
+    
+    elemento5();
+    elemento6();
+    elemento7();
+    elemento8();
+    elemento9();
+
+    function elemento0(){
+        document.getElementById("c0").style.display="flex";
+    }
+    
+    function elemento1(){
+        document.getElementById("c1").style.display="flex";
+    }
+    
+    function elemento2(){
+        document.getElementById("c2").style.display="flex";
+    }
+    
+    function elemento3(){
+        document.getElementById("c3").style.display="flex";
+    }
+    
+    function elemento4(){
+        document.getElementById("c4").style.display="flex";
+    }
+    
+    elemento0();
+    elemento1();
+    elemento2();
+    elemento3();
+    elemento4();
+}
+
+
+
+
+
 container.classList.add('containerPrincipal');
 
 /*const fondo=document.getElementById(".container");*/
@@ -57,23 +195,53 @@ function fondo4rem(){
     container.classList.remove('background-events');
     container.classList.add('containerPrincipal');
 }
-/*
+
 function fondo5(){
-    container.classList.add('background-history');
+    container.classList.remove('containerPrincipal');
+    container.classList.add('background-places');
+}
+
+function fondo5rem(){
+    container.classList.remove('background-places');
+    container.classList.add('containerPrincipal');
 }
 
 function fondo6(){
-    container.classList.add('background-history');
+    container.classList.remove('containerPrincipal');
+    container.classList.add('background-people');
+}
+
+function fondo6rem(){
+    container.classList.remove('background-people');
+    container.classList.add('containerPrincipal');
 }
 
 function fondo7(){
-    container.classList.add('background-history');
+    container.classList.remove('containerPrincipal');
+    container.classList.add('background-news');
+}
+
+function fondo7rem(){
+    container.classList.remove('background-news');
+    container.classList.add('containerPrincipal');
 }
 
 function fondo8(){
-    container.classList.add('background-history');
+    container.classList.remove('containerPrincipal');
+    container.classList.add('background-olimpics');
+}
+
+function fondo8rem(){
+    container.classList.remove('background-olimpics');
+    container.classList.add('containerPrincipal');
 }
 
 function fondo9(){
-    container.classList.add('background-history');
-}*/
+    container.classList.remove('containerPrincipal');
+    container.classList.add('background-map');
+}
+
+function fondo9rem(){
+    container.classList.remove('background-map');
+    container.classList.add('containerPrincipal');
+}
