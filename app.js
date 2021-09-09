eda7679ac680950cb4b70c869db5e72adcabcc95
AllIThinkAboutIsKarma <martinmarescr@gmail.com>
2021-09-09 18:52:00 +0200

    Summary fondos
    Se agregaron los fondos y se intentò el primer toogle al hacer hover en cada una de las cartas

---
+++ app.js
@@ -0,0 +1,50 @@
+/*var fondo = document.getElementById("#c0");*/
+/*$("#c0").click(() => {
+    $("#container").addClass("background-history");
+});*/
+
+/*const fondo=document.getElementById(".container");
+
+function fondo0(){
+    container.classList.remove('background-culture');
+    container.classList.add('background-history');
+}
+    
+function fondo1(){
+    container.classList.remove('background-gastronomy');
+    container.classList.add('background-culture');
+}
+
+function fondo2(){
+    container.classList.remove('background-arts');
+    container.classList.add('background-gastronomy');
+}
+
+function fondo3(){
+    container.classList.remove('background-events');
+    container.classList.add('background-arts');
+}
+
+function fondo4(){
+    container.classList.add('background-events');
+}
+
+function fondo5(){
+    container.classList.add('background-history');
+}
+
+function fondo6(){
+    container.classList.add('background-history');
+}
+
+function fondo7(){
+    container.classList.add('background-history');
+}
+
+function fondo8(){
+    container.classList.add('background-history');
+}
+
+function fondo9(){
+    container.classList.add('background-history');
+}
